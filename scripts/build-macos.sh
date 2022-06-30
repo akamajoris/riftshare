d30f60920f285e23c5ec467dcfeed449adf77149
Igor K <akamajoris@gmail.com>
2022-06-30 23:37:08 +0300

    2

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -9,13 +9,13 @@ echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-amd64.zip
 echo "Cleaning up"
 rm -rf ./build/bin/RiftShare.app
-echo "building on ARM64"
-wails build -platform darwin/arm64 -clean
+#echo "building on ARM64"
+#wails build -platform darwin/arm64 -clean
 #echo "Signing Package"
 #gon -log-level=info -log-json ./build/darwin/gon-sign.json
-echo "Zipping Package"
-ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-arm64.zip
-echo "Cleaning up"
-rm -rf ./build/bin/RiftShare.app
-echo "Notarizing Zip Files"
-gon -log-level=info ./build/darwin/gon-notarize.json
+#echo "Zipping Package"
+#ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-arm64.zip
+#echo "Cleaning up"
+#rm -rf ./build/bin/RiftShare.app
+#echo "Notarizing Zip Files"
+#gon -log-level=info ./build/darwin/gon-notarize.json
