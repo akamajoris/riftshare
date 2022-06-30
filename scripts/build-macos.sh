59e80fe84f2f8183cc2db4181e52549797bbce6f
Igor K <akamajoris@gmail.com>
2022-06-30 23:17:04 +0300

    kek

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -3,16 +3,16 @@
 export PATH=${PATH}:`go env GOPATH`/bin
 echo "building on AMD64"
 wails build -platform darwin/amd64 -clean
-echo "Signing Package"
-gon -log-level=info ./build/darwin/gon-sign.json
+#echo "Signing Package"
+# gon -log-level=info ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-amd64.zip
 echo "Cleaning up"
 rm -rf ./build/bin/RiftShare.app
 echo "building on ARM64"
 wails build -platform darwin/arm64 -clean
-echo "Signing Package"
-gon -log-level=info -log-json ./build/darwin/gon-sign.json
+#echo "Signing Package"
+#gon -log-level=info -log-json ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-arm64.zip
 echo "Cleaning up"
